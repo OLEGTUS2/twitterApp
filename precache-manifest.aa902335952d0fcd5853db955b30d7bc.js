cbfd23e6667f4337918ce144439c38d258bebdf5
Oleg <tysenkov.oleg@gmail.com>
2020-07-10 19:02:20 +0300

    Updates

---
+++ precache-manifest.aa902335952d0fcd5853db955b30d7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a71760a1fd7018a041eaf0385c2c57",
+    "url": "/twitterApp/index.html"
+  },
+  {
+    "revision": "bbb4c3c07554378fbc78",
+    "url": "/twitterApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4830247e9fc64acc6e65",
+    "url": "/twitterApp/static/css/main.556cc4d0.chunk.css"
+  },
+  {
+    "revision": "bbb4c3c07554378fbc78",
+    "url": "/twitterApp/static/js/2.8b74a6f5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/twitterApp/static/js/2.8b74a6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4830247e9fc64acc6e65",
+    "url": "/twitterApp/static/js/main.d462fe25.chunk.js"
+  },
+  {
+    "revision": "5737c2712544078a9f4c",
+    "url": "/twitterApp/static/js/runtime-main.1e8ed9b3.js"
+  }
+]);
